ac8a3771b00e1d6d51375355c01cdd687e1366a4
dmendozy <dmendozy@everis.com>
2020-07-17 15:40:58 -0500

    Update

--- src/main/java/com/transaction/transaction/TransactionApplication.java
+++ src/main/java/com/transaction/transaction/TransactionApplication.java
@@ -2,8 +2,12 @@ package com.transaction.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
+@EnableSwagger2WebFlux
+@EnableDiscoveryClient
 public class TransactionApplication {
 
 	public static void main(String[] args) {

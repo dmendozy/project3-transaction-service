b465712f8c5760dd13b69114f88f3e8c927b134c
dmendozy <dmendozy@everis.com>
2020-07-21 12:53:45 -0500

    update

--- src/main/java/com/transaction/transaction/TransactionApplication.java
+++ src/main/java/com/transaction/transaction/TransactionApplication.java
@@ -2,12 +2,10 @@ package com.transaction.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableSwagger2WebFlux
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class TransactionApplication {
 
 	public static void main(String[] args) {

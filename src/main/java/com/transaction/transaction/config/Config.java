0d4e3263bdbd9d3b15361aff5662b4ad7a220706
dmendozy <dmendozy@everis.com>
2020-07-21 12:54:03 -0500

    add config

---
+++ src/main/java/com/transaction/transaction/config/Config.java
@@ -0,0 +1,31 @@
+package com.transaction.transaction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class Config {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Bootcamp Project 2")
+                .description("Config Swagger for service")
+                .version("1")
+                .build();
+    }
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(this.apiInfo())
+                .enable(true)
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
